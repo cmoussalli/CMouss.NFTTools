1648ba4215d14a34495478ff30d95bde70963d74
Caesar Moussalli <cmoussalli@gmail.com>
2023-02-02 12:12:08 +0400

    Move to Github

---
+++ CMouss.NFTTools/Enums.cs
@@ -0,0 +1,28 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace CMouss.NFTTools
+//{
+
+//	public enum Chain
+//	{
+//		Ethereum = 1,
+//		Polygon = 2,
+//		BinanceSmartChain = 3,
+//		Cardano = 4,
+//		Solana = 5,
+//		Avalanche = 6,
+//		Tron = 7,
+//		EOS = 8,
+//		Tezos = 9
+//	}
+
+//	public enum MarketPlace
+//	{
+
+//	}
+
+//}
---
+++ CMouss.NFTTools/Tools/MarketPlaceTools.cs
@@ -0,0 +1,23 @@
+﻿//namespace CMouss.NFTTools
+//{
+//    public class MarketPlaceTools
+//    {
+
+//        public string GetChainName(string nftUrl)
+//        {
+//            string result = "";
+//            if (!String.IsNullOrEmpty(nftUrl))
+//            {
+
+//                //OpenSea
+//                if (nftUrl.ToLower().Contains("opensea.io"))
+//                {
+//                    string[] temps = nftUrl.Substring(nftUrl.IndexOf("assets") + 7).Split(@"/");
+//                    result = temps[0];
+//                }
+//            }
+//            return result;
+//        }
+
+//    }
+//}
